89f7df17685f25bb041390f14bad94079e6d8f0d
Miki_Sehrawat <sahiljaatbirohar@gmail.com>
2026-03-06 14:33:23 +0530

    Setup

---
+++ src/main/java/org/example/dnc/DncApplication.java
@@ -0,0 +1,13 @@
+package org.example.dnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DncApplication.class, args);
+    }
+
+}
